afaf161c9895ee14f5118400fa48ce49d440bb21
S-odland <49540258+S-odland@users.noreply.github.com>
2020-04-21 16:14:55 -0400

    HW2, DAC sine and triangle waves

---
+++ HW2.X/spi.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   spi.h
+ * Author: scott
+ *
+ * Created on April 20, 2020, 8:59 PM
+ */
+
+
+#ifndef SPI__H__
+#define SPI__H__
+
+void initSPI();
+unsigned char spi_io(unsigned char o);
+
+#endif // SPI__H__
