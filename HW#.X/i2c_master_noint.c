71deca62ec96c900e4eeb7deeab7047eb5f49734
S-odland <49540258+S-odland@users.noreply.github.com>
2020-05-19 21:58:54 -0400

    i2c - HW3

--- HW#.X/i2c_master_noint.c
+++ HW#.X/i2c_master_noint.c
@@ -1,12 +1,13 @@
 /* 
- * File:   main.c
+ * File:   i2c_master_noint.c
  * Author: scott
  *
- * Created on April 13, 2020, 10:53 PM
+ * Created on May 18, 2020, 5:11 PM
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "i2c_master_noint.h"
 #include<xc.h>           // processor SFR definitions
 #include<sys/attribs.h>  // __ISR macro
 
@@ -41,14 +42,71 @@
 #pragma config PMDL1WAY = OFF // allow multiple reconfigurations
 #pragma config IOL1WAY = OFF // allow multiple reconfigurations
 
+
+void i2c_master_setup(void) {
+    // using a large BRG to see it on the nScope, make it smaller after verifying that code works
+    // look up TPGD in the datasheet
+    I2C1BRG = 1000; // I2CBRG = [1/(2*Fsck) - TPGD]*Pblck - 2 (TPGD is the Pulse Gobbler Delay)
+    I2C1CONbits.ON = 1; // turn on the I2C1 module
+}
+
+void i2c_master_start(void) {
+    I2C1CONbits.SEN = 1; // send the start bit
+    while (I2C1CONbits.SEN) {
+        ;
+    } // wait for the start bit to be sent
+}
+
+void i2c_master_restart(void) {
+    I2C1CONbits.RSEN = 1; // send a restart 
+    while (I2C1CONbits.RSEN) {
+        ;
+    } // wait for the restart to clear
+}
+
+void i2c_master_send(unsigned char byte) { // send a byte to slave
+    I2C1TRN = byte; // if an address, bit 0 = 0 for write, 1 for read
+    while (I2C1STATbits.TRSTAT) {
+        ;
+    } // wait for the transmission to finish
+    if (I2C1STATbits.ACKSTAT) { // if this is high, slave has not acknowledged
+        // ("I2C1 Master: failed to receive ACK\r\n");
+        while(1){} // get stuck here if the chip does not ACK back
+    }
+}
+
+unsigned char i2c_master_recv(void) { // receive a byte from the slave
+    I2C1CONbits.RCEN = 1; // start receiving data
+    while (!I2C1STATbits.RBF) {
+        ;
+    } // wait to receive the data
+    return I2C1RCV; // read and return the data
+}
+
+void i2c_master_ack(int val) { // sends ACK = 0 (slave should send another byte)
+    // or NACK = 1 (no more bytes requested from slave)
+    I2C1CONbits.ACKDT = val; // store ACK/NACK in ACKDT
+    I2C1CONbits.ACKEN = 1; // send ACKDT
+    while (I2C1CONbits.ACKEN) {
+        ;
+    } // wait for ACK/NACK to be sent
+}
+
+void i2c_master_stop(void) { // send a STOP:
+    I2C1CONbits.PEN = 1; // comm is complete and master relinquishes bus
+    while (I2C1CONbits.PEN) {
+        ;
+    } // wait for STOP to complete
+}
+
 int main() {
 
     __builtin_disable_interrupts(); // disable interrupts while initializing things
 
-    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+    // set the CP0CONFIG register to indicate that kseg0 is cacheable (0x3)
     __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
 
-    // 0 data RAM access wait states
+    // 0 data  RAM access wait states
     BMXCONbits.BMXWSDRM = 0x0;
 
     // enable multi vector interrupts
@@ -60,36 +118,24 @@ int main() {
     // do your TRIS and LAT commands here
     TRISAbits.TRISA4 = 0;
     TRISBbits.TRISB4 = 1;
-    
     LATAbits.LATA4 = 0;
- 
+    
+    //i2c_master_setup();
+    
     __builtin_enable_interrupts();
-
+   
     while (1) {
-        // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
-        // remember the core timer runs at half the sysclk
-        
-        if (PORTBbits.RB4 == 0){
-            
+        if (PORTBbits.RB4 == 1){
             LATAbits.LATA4 = 1;
-            
             _CP0_SET_COUNT(0);
-            while(_CP0_GET_COUNT() < 24000000/1.5){}
-            
+            while(_CP0_GET_COUNT() < 2400000/1.5){}
             LATAbits.LATA4 = 0;
-            
             _CP0_SET_COUNT(0);
-            while(_CP0_GET_COUNT() < 24000000/1.5){}
-            
+            while(_CP0_GET_COUNT() < 2400000/1.5){}
             LATAbits.LATA4 = 1;
-            
             _CP0_SET_COUNT(0);
-            while(_CP0_GET_COUNT() < 24000000/1.5){}
-            
-            LATAbits.LATA4 = 0;
-            
+            while(_CP0_GET_COUNT() < 2400000/1.5){}
+            LATAbits.LATA4 = 0; 
         }
-         
-        
     }
 }
