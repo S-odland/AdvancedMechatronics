f730604e9ffbb7fbb61cf0aaa8dc67612f8f98b1
S-odland <49540258+S-odland@users.noreply.github.com>
2020-06-02 18:56:18 -0500

    start to hw5

---
+++ HW5.X/ws2812b.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   ws2812b.c
+ * Author: scott
+ *
+ * Created on June 2, 2020, 6:32 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ HW5.X/ws2812b.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   ws2812b.h
+ * Author: scott
+ *
+ * Created on June 2, 2020, 5:59 PM
+ */
+
+#ifndef WS2812B_H
+#define	WS2812B_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* WS2812B_H */
+
